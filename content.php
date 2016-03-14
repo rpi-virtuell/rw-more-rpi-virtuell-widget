5a0c9395a852f3763356aeed7e06c83d455e7ff3
Jörg Lohrer <info.empeiria@gmail.com>
2016-03-14 16:44:16 +0100

    + artothek und relilex

--- content.php
+++ content.php
@@ -15,11 +15,6 @@
 </style>
 <div class="rw-more-widget-button-area">
     <div class="rw-more-widget-button">
-        <a class="rw-more-widget-img-holder" href="http://blogs.rpi-virtuell.de" title="Deine Blogs">
-            <img class="rw-more-widget-img" src="<?php echo RW_More_Rpi_Virtuell_Widget::$plugin_url; ?>/images/blogs.png" width="80">
-        </a>
-    </div>
-    <div class="rw-more-widget-button">
         <a class="rw-more-widget-img-holder" href="http://news.rpi-virtuell.de" title="Neues und Wissenswertes">
             <img class="rw-more-widget-img" src="<?php echo RW_More_Rpi_Virtuell_Widget::$plugin_url; ?>/images/news.png" width="80">
         </a>
@@ -30,13 +25,28 @@
         </a>
     </div>
     <div class="rw-more-widget-button">
+        <a class="rw-more-widget-img-holder" href="http://blogs.rpi-virtuell.de" title="Deine Blogs">
+            <img class="rw-more-widget-img" src="<?php echo RW_More_Rpi_Virtuell_Widget::$plugin_url; ?>/images/blogs.png" width="80">
+        </a>
+    </div>
+    <div class="rw-more-widget-button">
         <a class="rw-more-widget-img-holder" href="http://www.rpi-virtuell.net/material" title="Material">
             <img class="rw-more-widget-img" src="<?php echo RW_More_Rpi_Virtuell_Widget::$plugin_url; ?>/images/material.png" width="80">
+        </a>
+    </div>
+       <div class="rw-more-widget-button">
+        <a class="rw-more-widget-img-holder" href="http://blogs.rpi-virtuell.de/artothek" title="rpi!-Artothek">
+            <img class="rw-more-widget-img" src="<?php echo RW_More_Rpi_Virtuell_Widget::$plugin_url; ?>/images/artothek.png" width="80">
         </a>
     </div>
     <div class="rw-more-widget-button">
         <a class="rw-more-widget-img-holder" href="http://open.rpi-virtuell.de" title="Offene Kurse">
             <img class="rw-more-widget-img" src="<?php echo RW_More_Rpi_Virtuell_Widget::$plugin_url; ?>/images/courses.png" width="80">
+        </a>
+    </div>
+      <div class="rw-more-widget-button">
+        <a class="rw-more-widget-img-holder" href="http://relilex.de" title="relilex">
+            <img class="rw-more-widget-img" src="<?php echo RW_More_Rpi_Virtuell_Widget::$plugin_url; ?>/images/relilex.png" width="80">
         </a>
     </div>
     <div class="rw-more-widget-button">
